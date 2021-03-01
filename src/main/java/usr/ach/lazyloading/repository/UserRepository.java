98e0dd2e25b1c7982317996f6f969fcb6c69ae13
Antonio Chavez <antonioluischavez@gmail.com>
2021-03-01 23:06:34 +0100

    Add JOIN FETCH queries

--- src/main/java/usr/ach/lazyloading/repository/UserRepository.java
+++ src/main/java/usr/ach/lazyloading/repository/UserRepository.java
@@ -1,7 +1,21 @@
 package usr.ach.lazyloading.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import usr.ach.lazyloading.model.User;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Override
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.orders WHERE u.id = :id")
+    Optional<User> findById(@Param("id") final Long id);
+
+
+    @Override
+    @Query(value = "SELECT DISTINCT u FROM User u LEFT JOIN FETCH u.orders")
+    List<User> findAll();
 }
